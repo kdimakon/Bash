3cc1895fe412d9bba81e9cebc09dd1367be18ba0
Norma <norma@mail.com>
2023-02-13 17:57:33 +0300

    monitoring.sh

---
+++ monitoring.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+function cpu ()
+{
+      CORES=$(nproc)
+      LOAD_AVERAGE=$(uptime | awk '{print $8}' | sed 's/,//g')
+      LOAD_PERCENT=$( echo "$LOAD_AVERAGE * 100" | bc -l)
+
+      echo -e "-------------------- CPU USAGE FOR MINUTE --------------------\n"
+      echo -e "NUMBER OF CORES:\t\t\t $CORES\n"
+      echo -e "LOAD CPU:\t\t\t $LOAD_PERCENT%\n"
+      echo -e "--------------------------------------------------------------\n"
+
+}
+
+function mem ()
+{
+
+      TOTAL_MEM=$(free -m | awk 'NR == 2{print $2}')
+      USED_MEM=$(free -m | awk 'NR == 2{print $3}')
+      USED_MEM_PERCENT=$( echo "$USED_MEM * 100 / $TOTAL_MEM" | bc -l)
+
+      TOTAL_SWAP=$(free -m | awk 'NR == 3{print $2}')
+      USED_SWAP=$(free -m | awk 'NR == 3{print $3}')
+
+      AVAILABLE_MEM=$(free -m | awk 'NR == 1{print $7}')
+
+      echo -e "-------------------- MEM USAGE FOR MINUTE --------------------\n"
+      echo -e "\nAVAILABLE MEMORY:\t\t\t $AVAILABLE_MEM\n"
+      echo -e "TOTAL MEMORY:\t\t\t $TOTAL_MEM\n"
+      echo -e "USED MEMORY:\t\t\t $USED_MEM\n"
+      echo -e "TOTAL SWAP:\t\t\t $TOTAL_SWAP\n"
+      echo -e "USED SWAP:\t\t\t $USED_SWAP\n"
+      echo -e "\nUSED MEMORY PERCENT:\t\t\t $USED_MEM_PERCENT%\n"
+      echo -e "--------------------------------------------------------------\n"
+}
+
+function disk_usage () 
+{
+      ROOT_DISK_USAGE=$(df -h | grep -w '/' | awk '{print $5}')
+      ROOT_DISK_AVAIL=$(df -h | grep -w '/' | awk '{print $4}')
+
+      echo -e "-------------------- DISK USAGE FOR MINUTE -------------------\n"
+      echo -e "ROOT DISK USAGE:\t\t\t $ROOT_DISK_USAGE\n"
+      echo -e "ROOT DISK AVAILABLE:\t\t\t $ROOT_DISK_AVAIL\n"
+
+}
+
+function main ()
+{
+  mem
+  cpu
+  disk_usage
+}
+main

32c533fcfb04eddc594274cc6b12ebe98d3217e9
Norma <norma@mail.com>
2023-02-15 15:05:29 +0300

    BACKUP

---
+++ Scripts_for_Backup/Full_Backup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source /root/source_for_backup.txt
+
+function backup_server ()
+{
+    
+    while read -r ip
+    do
+        ping -c 1 "$ip" 2>/dev/null 1>/dev/null
+        if [ "$?" = 0 ]
+        then
+            S_USERNAME="root"
+            BACKUP_FILE="$S_NAME::$CURR_DATE.tar.gz"
+            S_NAME=$(ssh -n $S_USERNAME@"$ip" 'hostname')
+            
+            ssh -n $S_USERNAME@"$ip" "tar czf - --one-file-system $W_BACKUP | ssh $FS_USERNAME@$FS_IP "cat > "$DIR"/"$BACKUP_FILE"""
+
+            
+            echo "$CURR_DATE: Server $S_NAME ($ip):  Backup was done " >> /root/log_backup.txt
+        else
+            mail -s "Server $ip isn't available" wchoy@mailto.plus
+            echo "$CURR_DATE: Server $ip: Server isn't available " >> /root/log_backup.txt
+        fi
+    done </root/ip.txt
+    
+}
+
+backup_server 2>/dev/null &
